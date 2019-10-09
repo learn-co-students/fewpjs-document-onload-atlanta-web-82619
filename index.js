60ace8ea67db75e141de492dbc87e1298f937005
Raza <rtshareef@gmail.com>
2019-10-09 18:57:58 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    let new2 = document.getElementById("text")
+    // const para = document.createElement('p')
+    new2.textContent = 'This is really cool!'
+    // new2.replaceChild(para)
+  });
